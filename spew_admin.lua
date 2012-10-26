e1ac2bffa11de37b31619d2ec698bdf63489ddcd
Kriss <kriss@XIXs.com>
2012-10-27 00:22:56 +0100

    reg ban mudded ips on request

--- spew_admin.lua
+++ spew_admin.lua
@@ -232,7 +232,33 @@ end
 -----------------------------------------------------------------------------
 function is_mud(name)
 
+	for i,v in ipairs( get_shared_names_by_ip(user.name) or {} ) do
+	
+		if is_mud(v) then
+			if not data.mud_names[ string.lower(user.name) ] then -- if not already set
+				data.mud_names[ string.lower(user.name) ]=true
+log(v,"mud",user.name) -- log the mudding of
+			end
+			break
+		end
+	
+	end
+		
+		
 	if data.mud_names[ string.lower(name) ] then return true end
+	
+	return false
+end
+
+function is_mudip(name)
+
+	for i,v in ipairs( get_shared_names_by_ip(name) or {name} ) do
+	
+		if is_mud(v) then
+			return true
+		end
+	
+	end
 	
 	return false
 end
