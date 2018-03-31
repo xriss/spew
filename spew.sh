360b8fe418c092909d86f0c31a3bf6da7ddab8a2
kriss <kriss@xixs.com>
2018-03-31 03:04:18 +0200

    .

--- spew.sh
+++ spew.sh
@@ -1,1 +1,2 @@
+cd /devcake/spew
 nix/bin/lua -e "package.cpath='./nix/lib/?.so;'..package.cpath;package.path='./nix/share/?.lua;'..package.path;" spewd.lua 1>>spew.log 2>>spew.err </dev/null &
--- spewl.sh
+++ spewl.sh
@@ -1,2 +1,2 @@
-cd /home/wet/hg/spew
+cd /devcake/spew
 nix/bin/lua -e "package.cpath='./nix/lib/?.so;'..package.cpath;package.path='./nix/share/?.lua;'..package.path;" spewl.lua
